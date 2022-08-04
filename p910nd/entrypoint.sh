50ee6b97bdbd20f7009850b3219e14422e65341a
justin-himself <justin.zhang.edu@gmail.com>
2022-08-05 01:41:31 +0800

    fi p910nd

--- p910nd/entrypoint.sh
+++ p910nd/entrypoint.sh
@@ -1,23 +1,23 @@
+#!/bin/sh
+
 echo >&2 "       ___  _  ___            _ "
 echo >&2 " _ __ / _ \/ |/ _ \ _ __   __| |"
-echo >&2 "| '_ \ (_) | | | | | '_ \ / _` |"
+echo >&2 "| '_ \ (_) | | | | | '_ \ / _\` |"
 echo >&2 "| |_) \__, | | |_| | | | | (_| |"
 echo >&2 "| .__/  /_/|_|\___/|_| |_|\__,_|"
 echo >&2 "|_|                             "
 echo >&2 "-----------------------------------\n\n"
 
-
 if [ $# -eq 0 ]; then
-    args="-f $PRINTER_DEVICE "
+    args="-f $PRINTER_DEVICE"
     if [ $BIDIRECTIONAL -eq 1 ]; then
-        echo args+="-b "
+        args="$args -b "
     fi
     if [ $DEBUG_INFO -eq 1 ]; then
-        echo args+="-d "
+        args="$args -d "
     fi
 else
     args=$@
 fi
 
-echo "p910nd $args"
-/usr/bin/p910nd $args
+p910nd $args
