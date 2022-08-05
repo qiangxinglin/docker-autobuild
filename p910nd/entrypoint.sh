cbcc599961722ceebdd6cd706b1e7e45a5f795fb
Justin Zhang <73123028+justin-himself@users.noreply.github.com>
2022-08-05 11:36:08 +0800

    Update entrypoint.sh

--- p910nd/entrypoint.sh
+++ p910nd/entrypoint.sh
@@ -1,23 +1,13 @@
-#!/bin/sh
-
-echo >&2 "       ___  _  ___            _ "
-echo >&2 " _ __ / _ \/ |/ _ \ _ __   __| |"
-echo >&2 "| '_ \ (_) | | | | | '_ \ / _\` |"
-echo >&2 "| |_) \__, | | |_| | | | | (_| |"
-echo >&2 "| .__/  /_/|_|\___/|_| |_|\__,_|"
-echo >&2 "|_|                             "
-echo >&2 "-----------------------------------\n\n"
-
-if [ $# -eq 0 ]; then
-    args="-f $PRINTER_DEVICE"
-    if [ $BIDIRECTIONAL -eq 1 ]; then
-        args="$args -b "
-    fi
-    if [ $DEBUG_INFO -eq 1 ]; then
-        args="$args -d "
-    fi
+#!/bin/bash
+if $@ == *"-d"*; then
+  echo >&2 "       ___  _  ___            _ "
+  echo >&2 " _ __ / _ \/ |/ _ \ _ __   __| |"
+  echo >&2 "| '_ \ (_) | | | | | '_ \ / _\` |"
+  echo >&2 "| |_) \__, | | |_| | | | | (_| |"
+  echo >&2 "| .__/  /_/|_|\___/|_| |_|\__,_|"
+  echo >&2 "|_|                             "
+  echo >&2 "-----------------------------------\n\n"
 else
-    args=$@
+  echo >&2 "p910nd started."
 fi
-
-exec "p910nd $args"
+p910nd $@ >&2
