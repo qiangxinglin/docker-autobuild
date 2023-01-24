f8495003164915aaef8ebd73ce66f9ef64b38f75
justin-himself <justin.zhang.edu@gmail.com>
2023-01-24 14:49:40 +0800

    fix

--- i2pd/rootfs/entrypoint.sh
+++ i2pd/rootfs/entrypoint.sh
@@ -9,15 +9,15 @@ echo "         |_|          "
 # copy config for first time installtion
 if [ ! -e /config/i2pd ]; then
 
-    echo -n "\nCopying new files to directory."
+    echo "Copying new files to directory."
 
     if [ -n "$(find /config -mindepth 1 -maxdepth 1)" ]; then
 			echo >&2 "Directory not empty!"
             exit 
 	fi
 
-    cp -r /config.default /config
+    cp -r /config.default/ /config/
 
 fi
 
-exec "/usr/local/bin/i2pd --datadir=/config"
+/usr/local/bin/i2pd --datadir=/config
